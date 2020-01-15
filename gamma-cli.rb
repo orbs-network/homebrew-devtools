7d00a8f5b46ef1b5c0a4348b23046ca5ceea4494
Jonathan Levison <jonathan.levison@gmail.com>
2020-01-15 21:34:30 +0200

    gamma-cli v0.10.0

--- gamma-cli.rb
+++ gamma-cli.rb
@@ -4,11 +4,11 @@ class GammaCli < Formula
   desc "Command line tools for Orbs Gamma personal blockchain"
   homepage "https://github.com/orbs-network/orbs-contract-sdk"
   if OS.mac?
-    url "https://github.com/orbs-network/gamma-cli/releases/download/v0.9.0/gammacli-mac-v0.9.0.tar.gz"
-    sha256 "5f827591b4c538e53711340bac40fc664e06c90f09a823a09ce96dfd141aa16a"
+    url "https://github.com/orbs-network/gamma-cli/releases/download/v0.10.0/gammacli-mac-v0.10.0.tar.gz"
+    sha256 "8f50a52ab54e16b62d0c0202be3bdb3205e266c787e4a0d08342c2d1885a3155"
   elsif OS.linux?
-    url "https://github.com/orbs-network/gamma-cli/releases/download/v0.9.0/gammacli-linux-x86-64-v0.9.0.tar.gz"
-    sha256 "7d100a98713106075a2b250c94bb147f5037e6c727d39b7c9407362e0deb647c"
+    url "https://github.com/orbs-network/gamma-cli/releases/download/v0.10.0/gammacli-linux-x86-64-v0.10.0.tar.gz"
+    sha256 "816ee6f48f47fbda19f00d503e084b582089fece932e464eabea41a41d3dcf03"
   else
     ohdie "Your operating system is not supported by this formula, if you feel this is a mistake please contract Orbs"
   end  
@@ -16,11 +16,11 @@ class GammaCli < Formula
 
   devel do
     if OS.mac?
-      url "https://github.com/orbs-network/gamma-cli/releases/download/v0.9.0/gammacli-mac-v0.9.0.tar.gz"
-      sha256 "5f827591b4c538e53711340bac40fc664e06c90f09a823a09ce96dfd141aa16a"
+      url "https://github.com/orbs-network/gamma-cli/releases/download/v0.10.0/gammacli-mac-v0.10.0.tar.gz"
+      sha256 "8f50a52ab54e16b62d0c0202be3bdb3205e266c787e4a0d08342c2d1885a3155"
     elsif OS.linux?
-      url "https://github.com/orbs-network/gamma-cli/releases/download/v0.9.0/gammacli-linux-x86-64-v0.9.0.tar.gz"
-      sha256 "7d100a98713106075a2b250c94bb147f5037e6c727d39b7c9407362e0deb647c"
+      url "https://github.com/orbs-network/gamma-cli/releases/download/v0.10.0/gammacli-linux-x86-64-v0.10.0.tar.gz"
+      sha256 "816ee6f48f47fbda19f00d503e084b582089fece932e464eabea41a41d3dcf03"
     else
       ohdie "Your operating system is not supported by this formula, if you feel this is a mistake please contract Orbs"
     end  
